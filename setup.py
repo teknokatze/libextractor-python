cbc832eb77362fcd71c8571a89dddc08bc714c5b
Nils Gillmann <gillmann@infotropique.org>
2018-06-08 05:43:11 +0000

    whitespace only.
    Signed-off-by: Nils Gillmann <gillmann@infotropique.org>

--- setup.py
+++ setup.py
@@ -19,26 +19,26 @@ setup(
     description = "Python bindings for GNU libextractor",
     license = "GNU GPLv3+",
     keywords = "libextractor binding tag metadata",
-    url = "https://www.gnu.org/s/libextractor/",  
+    url = "https://www.gnu.org/s/libextractor/",
     long_description="""libextractor is a simple library for keyword extraction.  libextractor
 does not support all formats but supports a simple plugging mechanism
 such that you can quickly add extractors for additional formats, even
 without recompiling libextractor. libextractor typically ships with a
 dozen helper-libraries that can be used to obtain keywords from common
-file-types.  
+file-types.
 
 libextractor is a part of the GNU project (https://www.gnu.org/).""",
-    
+
     classifiers=['Development Status :: 3 - Alpha',
-		 'Intended Audience :: Developers',
-		 'License :: OSI Approved :: GNU General Public License (GPL)',
-		 'Operating System :: OS Independent',
-		 'Operating System :: MacOS :: MacOS X',
-		 'Operating System :: Microsoft :: Windows',
-		 'Operating System :: POSIX',
-		 'Topic :: System :: Filesystems',
-		 'Topic :: Text Processing :: Filters'],
-    
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: GNU General Public License (GPL)',
+                 'Operating System :: OS Independent',
+                 'Operating System :: MacOS :: MacOS X',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Topic :: System :: Filesystems',
+                 'Topic :: Text Processing :: Filters'],
+
     platforms=['windows', 'Linux', 'MacOS X', 'Solaris', 'FreeBSD'],
-    
+
 )
