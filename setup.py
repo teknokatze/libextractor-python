6ffafec3d8131906cde8ed0ec4a51c2a6f91afb3
Nils Gillmann <gillmann@infotropique.org>
2018-06-17 13:18:06 +0000

    +
    Signed-off-by: Nils Gillmann <gillmann@infotropique.org>

--- setup.py
+++ setup.py
@@ -1,35 +1,32 @@
 #!/bin/env/python
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
 
 setup(
     name = "Extractor",
     version = "1.7",
 
-    packages = ['libextractor'],
-    scripts = ['examples/extract.py'],
-
+    packages = find_packages(),
     entry_points = { "console_scripts": [ "extract.py = libextractor.examples.__main__:main", ] },
 
-    # install_requires = ['ctypes >= 0.9'],
-
-    # metadata for upload to PyPI
     author = "Bader Ladjemi, Christian Grothoff, Nils Gillmann (ng0)",
     author_email = "libextractor@gnu.org",
     description = "Python bindings for GNU libextractor",
     license = "GNU GPLv3+",
     keywords = "libextractor binding tag metadata",
     url = "https://www.gnu.org/s/libextractor/",
-    long_description="""libextractor is a simple library for keyword extraction.  libextractor
+    long_description="""
+libextractor is a simple library for keyword extraction.  libextractor
 does not support all formats but supports a simple plugging mechanism
 such that you can quickly add extractors for additional formats, even
 without recompiling libextractor. libextractor typically ships with a
 dozen helper-libraries that can be used to obtain keywords from common
 file-types.
 
-libextractor is a part of the GNU project (https://www.gnu.org/).""",
+libextractor is a part of the GNU project (https://www.gnu.org/).
+    """,
 
     classifiers=['Development Status :: 3 - Alpha',
                  'Intended Audience :: Developers',
@@ -40,6 +37,5 @@ libextractor is a part of the GNU project (https://www.gnu.org/).""",
                  'Operating System :: POSIX',
                  'Topic :: System :: Filesystems',
                  'Topic :: Text Processing :: Filters'],
-
     platforms=['windows', 'Linux', 'MacOS X', 'Solaris', 'FreeBSD'],
 )
